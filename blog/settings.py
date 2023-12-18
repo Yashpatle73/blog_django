80b3b4975fbb26ac5f8f2282ae4ded45bf27ebf7
Yash Patle <yashpatle89@gmail.com>
2023-12-19 00:08:18 +0530

    Upadated to mysql database

--- blog/settings.py
+++ blog/settings.py
@@ -75,8 +75,17 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       # 'ENGINE': 'django.db.backends.sqlite3',
+       # 'NAME': BASE_DIR / 'db.sqlite3',
+       'ENGINE': 'django.db.backends.mysql',
+       'OPTIONS': {
+            'init_command': 'SET default_storage_engine=INNODB',
+        }, 
+        'NAME': 'blogdb',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3308',
     }
 }
 
