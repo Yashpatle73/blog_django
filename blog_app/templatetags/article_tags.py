793d52bd2ab17d11e7a69f817e4778a2f877d301
Yash Patle <yashpatle89@gmail.com>
2023-12-19 15:52:21 +0530

    Number/latest article

---
+++ blog_app/templatetags/article_tags.py
@@ -0,0 +1,20 @@
+from django import template
+from ..models import Article
+
+
+register =template.Library()
+
+
+@register.simple_tag
+def total_articles():
+    return Article.publishedArticles.count()
+    pass
+
+
+@register.inclusion_tag('blog_app/latest_articles.html')
+def show_latest_articles(count=3):
+    latest_articles = Article.publishedArticles.order_by('-publish')[:count]
+    return {'latest_articles': latest_articles}
+    pass
+
+
