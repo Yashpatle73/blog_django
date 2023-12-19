d760b931bb81ead66d2184300c18fb20d8cae425
Yash Patle <yashpatle89@gmail.com>
2023-12-19 11:08:09 +0530

    pagination part

--- blog_app/views.py
+++ blog_app/views.py
@@ -3,10 +3,19 @@ from .models import Article
 from django.http import HttpResponse
 from django.shortcuts import render , get_object_or_404
 from django.http import Http404
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 # Create your views here.
 def list_of_articles(request):
     articles=Article.publishedArticles.all()
     #print(artcles)
+    paginator=Paginator(articles,3)
+    page_number=request.GET.get('page',1)
+    try:
+        articles=paginator.page(page_number)
+    except EmptyPage:
+        articles=paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        articles=paginator.page(1)
 
     return render (request ,'blog_app/list.html',{'articles':articles})
     pass
