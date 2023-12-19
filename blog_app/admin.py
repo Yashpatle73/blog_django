d8ce5270ed77b8ecde65f31cbfdf1435c3822f72
Yash Patle <yashpatle89@gmail.com>
2023-12-19 11:52:39 +0530

    Comment section

--- blog_app/admin.py
+++ blog_app/admin.py
@@ -16,3 +16,10 @@ class ArticleAdmin(admin.ModelAdmin):
     
     pass
 
+@admin.register(Comment)
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display=['name','email','article','created','active']
+    list_filter=['active','created','updated']
+    search_fields=['name','email','body']
+
--- blog_app/views.py
+++ blog_app/views.py
@@ -2,8 +2,10 @@
 from .models import Article 
 from django.http import HttpResponse
 from django.shortcuts import render , get_object_or_404
-from django.http import Http404
+from django.http import Http404 
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
+from django.views.decorators.http import require_POST
+from .forms import CommentForm
 # Create your views here.
 def list_of_articles(request):
     articles=Article.publishedArticles.all()
@@ -29,11 +31,36 @@ def article_details(request,year,month,day,article):
                                   publish__month=month,
                                   publish__day=day
                                   )
+        comments=article.comments.filter(active=True)# List of active comments for this article
+        # Form for users to write comment
+        form = CommentForm()
     except Article.DoesNotExist:
         raise Http404("No Article Found")
     
     return render (request , 'blog_app/detail.html',{'article':article})
     pass
 
+@require_POST
+def comment_for_article(request, article_id):
 
+    # get the article by article_id
+    article = get_object_or_404(Article, id = article_id, status=Article.Status.PUBLISHED)
+    comment = None
+    
+    # A comment form
+    form = CommentForm(data=request.POST)
+
+    if form.is_valid():
+        # Create a Comment object before saving it to the database
+        comment = form.save(commit=False)
+
+        # Assign the article to the comment
+        comment.article = article
+        # Save the comment to the database
+        comment.save()
+        pass
+
+    return render(request, 'blog_app/comment.html', {'article': article, 'form': form, 'comment': comment})
+
+    pass
 
